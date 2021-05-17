735d4c6c19fa1c2ff74b7e6108e986a7a344c28f
David AW Barton <david.barton@bristol.ac.uk>
2021-05-17 16:24:49 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/DynamicalSystemsToolkit.jl
@@ -0,0 +1,5 @@
+module DynamicalSystemsToolkit
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DynamicalSystemsToolkit
+using Test
+
+@testset "DynamicalSystemsToolkit.jl" begin
+    # Write your tests here.
+end
