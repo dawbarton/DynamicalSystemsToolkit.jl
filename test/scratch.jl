134eb87e6573ecaf5c5d8b476875c6d221e17999
David AW Barton <david.barton@bristol.ac.uk>
2021-06-28 13:46:05 +0100

    More testing

--- test/scratch.jl
+++ test/scratch.jl
@@ -2,7 +2,7 @@ using DynamicalSystemsToolkit
 using ModelingToolkit
 using NonlinearSolve
 
-function testsystem()
+function hopf()
     @parameters t β σ
     @variables y₁(t) y₂(t)
     D = Differential(t)
@@ -15,11 +15,22 @@ function testsystem()
         [y₁, y₂],
         [β, σ];
         defaults=[β => 1, σ => -1],
-        name=:Hopf,
+        name=:hopf,
     )
 end
 
-coll = CollocationSystem(testsystem(), 5, 3)
+function hopf_test()
+    coll = CollocationSystem(hopf(), 1, 3)
+    add_boundarycondition!(coll, (u₀, t₀, u₁, t₁, p) -> u₀)  # periodicity
+    add_boundarycondition!(coll, (u₀, t₀, u₁, t₁, p) -> [0 ~ t₀, 0 ~ t₁ - 2π])  # start time and phase condition
+    sol = t -> (cos(t), sin(t))
+    ics = Dict(generate_initialconditions(coll, sol, (0, 2π)))
+    fun = NonlinearFunction(NonlinearSystem(coll))
+
+    res = fun(getindex.(Ref(ics), states(coll)), getindex.(Ref(ModelingToolkit.get_defaults(coll)), parameters(coll)))
+
+    return (coll, ics, fun, res)
+end
 
 function test2()
     @variables x y
